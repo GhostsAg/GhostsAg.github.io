e58b700bbca19c4a19257b225c4b3e4d83c34309
GhostsAg <Andres.Ledesma9511@gmail.com>
2020-04-04 17:50:45 -0500

    Updates

---
+++ precache-manifest.9b91ac192fe2eb526d4b0794d1df2e74.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "557a0e0273b31995812cb255e3559c50",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a2fbb1851eaa02500e14",
+    "url": "/static/css/main.d9ead71a.chunk.css"
+  },
+  {
+    "revision": "025e34ef8d37aba7adb0",
+    "url": "/static/js/2.4eaf028d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.4eaf028d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2fbb1851eaa02500e14",
+    "url": "/static/js/main.6717473b.chunk.js"
+  },
+  {
+    "revision": "4c15ceb5499f6abee85b",
+    "url": "/static/js/runtime-main.eb07c764.js"
+  }
+]);
