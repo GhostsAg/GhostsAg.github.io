d9d6ce5f8f0c59bb92bd5e34b368bb269a97fc1d
GhostsAg <Andres.Ledesma9511@gmail.com>
2020-04-04 17:22:31 -0500

    about section.

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -5,22 +5,20 @@ function About() {
     return (
         <div className = "row">
             <div className="col s12">
-                <h3>About Me</h3>
-                <ul>
-                    <li>I aspire to become a programs design engineer</li>
-                    <li>Programming Interest:</li>
-                    <ul>
-                        <li>Computer Vision</li>
-                        <li>Machine Learning</li>
-                        <li>Data Manipulation</li>
-                    </ul>
+            <p>I aspire to be a programs design engineer.</p>
+                <ul id="list">
+                Programming Interest:
+                    <li>Computer Vision</li>
+                    <li>Machine Learning</li>
+                    <li>Data Manipulation</li>
                     <li>Languages I am familiar with:</li>
-                    <ul>
-                        <li>C/C++</li>
-                        <li>Matlab</li>
-                        <li>Javascript</li>
-                        <li>Python</li>
-                    </ul>
+                </ul>    
+                <ul id="list">
+                    Programming Languages:
+                    <li>C/C++</li>
+                    <li>Matlab</li>
+                    <li>Javascript</li>
+                    <li>Python</li>
                 </ul>
                 <p>In my off time I like to learn and talk about creative topics. Most of the time they have to do with the financial world or engineering.</p>
             </div>
