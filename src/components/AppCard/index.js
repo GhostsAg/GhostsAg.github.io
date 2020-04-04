1614a76042b10a5fa128238b3ec584583033bcac
GhostsAg <Andres.Ledesma9511@gmail.com>
2020-04-04 17:45:04 -0500

    apps section.

--- src/components/AppCard/index.js
+++ src/components/AppCard/index.js
@@ -9,12 +9,12 @@ function AppCard(props) {
                 <img className="activator" src={props.img} />
             </div>
             <div className="card-content">
-                <span className="card-title activator grey-text text-darken-4">{props.appName}<i className="material-icons right">more_vert</i></span>
+                <span id="appName" className="card-title activator grey-text text-darken-4">{props.appName}<i className="material-icons right">more_vert</i></span>
                 <p><a href={props.link}>Open App</a></p>
             </div>
             <div className="card-reveal">
-                <span className="card-title grey-text text-darken-4">{props.appName}<i className="material-icons right">close</i></span>
-                <p>{props.info}</p>
+                <span id="appName" className="card-title grey-text text-darken-4">{props.appName}<i className="material-icons right">close</i></span>
+                <p id="info">{props.info}</p>
                 <p><a href={props.repo}>Repo</a></p>
             </div>
         </div>
--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -5,9 +5,8 @@ import "./style.css";
 function Portfolio() {
     return (
         <div className = "row">
-            <h3>My Apps</h3>
             <div className = "col s6">
-                <AppCard img="" 
+                <AppCard img={process.env.PUBLIC_URL + "/images/Employee-Database.png"} 
                     appName="Employee Databse"
                     link="https://github.com/GhostsAg/EmployeeTracker"
                     info="Command-line application that allows you to input and manipulate employee data by departments, roles, and salary."
@@ -15,15 +14,15 @@ function Portfolio() {
                 />
             </div>
             <div className = "col s6">
-                <AppCard img=""
+                <AppCard img={process.env.PUBLIC_URL + "/images/Contract-Bay.png"}
                     appName="Contract Bay"
                     link="https://polar-island-40995.herokuapp.com/"
-                    info="Contract accessibility web application to close the gap between supplier and manufacturer/retailer."
+                    info="Contract accessibility web application to close the gap between supplier and manufacturer."
                     repo="https://github.com/robertharris95/Project-2"
                 />
             </div>
             <div className = "col s6">
-                <AppCard img=""
+                <AppCard img={process.env.PUBLIC_URL + "/images/WeatherDash.png"}
                     appName="Weather Dashboard"
                     link="https://ghostsag.github.io/WeatherDashboard/"
                     info="Weather forecast web application. Get information about the upcoming 5-days of any city."
