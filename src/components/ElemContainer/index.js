e14344256bc75835935116190e9cb5a9cbfe4d09
GhostsAg <Andres.Ledesma9511@gmail.com>
2020-04-05 15:52:17 -0500

    deploy.

--- src/components/ElemContainer/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "./style.css";
-
-// Add ElemContainer always as props to use the imported style.css
-function ElemContainer(props) {
-  return <div className={props.addClass}>{props.children}</div>;
-}
-
-export default ElemContainer;
